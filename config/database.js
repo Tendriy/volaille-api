efea6532884ef6cb7d1a9c0186bdd5453315c08a
Tendriy <anissa.andrianindrina@student.passerellesnumerique.org>
2026-05-02 08:00:50 +0300

    feat: changer la configuration de pg

--- config/database.js
+++ config/database.js
@@ -2,20 +2,17 @@
 const { Pool } = require('pg');
 
 const client = new Pool({
-    host: process.env.DB_HOST,    // ou '127.0.0.1'
-     port: process.env.DB_PORT || 5432,// Port par défaut PostgreSQL
-    user: process.env.DB_USER,     // Votre utilisateur
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    max: 10,               // Maximum de connexions dans le pool
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    statement_timeout: 10000, 
+    query_timeout: 10000,
+    idle_in_transaction_session_timeout: 10000
 });
 
 // Tester la connexion
 client.connect((err, c, release) => {
     if (err) {
-        console.error( err.stack);
+        console.error(err.stack);
     } else {
         release();
     }
